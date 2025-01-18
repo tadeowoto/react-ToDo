f825b9452caa460a9725ff4ede29e81ded77be03
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-01-18 12:50:32 -0300

    colors v added

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,7 +1,7 @@
 
 const CreateToDo = () => {
   return (
-    <button>+</button>
+    <button>Add Task</button>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        bgColor: '#008080',
+        navColor: '#000080',
+        cardBg: '#C0C0C0'
+      }
+    },
   },
   plugins: [],
 }
